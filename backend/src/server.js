158322e4e994afe4ba38439bc6272f4c3ba864f3
williamcostaramos <wulliamcostaramos@gmail.com>
2019-10-07 02:14:18 -0300

    alteracao no metodo de retorno do express

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,16 @@ const express= require('express');
 
 const app= express();
 app.get('/', (req, res)=>{
-return res.json({message: "hello wold"});
+return res.json({message: "Server ok"});
 
-})
+});
+app.set("json spaces",4);
+app.get('/tasks',(req, res)=>{
+    res.json({
+        tasks: [
+            {"title": "fazer Compras", "status": "feito"},
+            {"title": "Começa a Estudar", "status": "feito"}
+        ]
+    })
+});
 app.listen(3000);
